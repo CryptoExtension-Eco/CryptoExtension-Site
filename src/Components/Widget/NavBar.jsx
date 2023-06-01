f453e0f5ebd2413005af314f7dca2e80a711a77f
napotwiixe <julien.milants@gmail.com>
2023-06-01 16:24:56 +0200

    start responsive

--- src/Components/Widget/NavBar.jsx
+++ src/Components/Widget/NavBar.jsx
@@ -3,15 +3,15 @@ import '../../scss/Pages/Widgets/Navbar.scss'
 const NavBar = () => {
   return (
     <>
-      <header class="header">
-            <a href="#home" class="header__logo">CryptoExtension</a>
-            <ul class="header__navigation">
-                <li class="header__navigation-item"><a href="#Home" rel="noopener noreferrer" class="header__navigation-link">Home</a></li>
-                <li class="header__navigation-item"><a href="#Dashboard" rel="noopener noreferrer" class="header__navigation-link">Dashboard</a></li>
-                <li class="header__navigation-item"><a href="#Download" rel="noopener noreferrer" class="header__navigation-link">Download</a></li>
-                {/* <li class="header__navigation-item"><a href="#login" target="_blank" rel="noopener noreferrer" class="header__navigation-link">Login</a></li> */}
+      <nav className="header">
+            <a href="#home" className="header__logo">CryptoExtension</a>
+            <ul className="header__navigation">
+                <li className="header__navigation-item"><a href="#Home" rel="noopener noreferrer" className="header__navigation-link">Home</a></li>
+                <li className="header__navigation-item"><a href="#Dashboard" rel="noopener noreferrer" className="header__navigation-link">Dashboard</a></li>
+                <li className="header__navigation-item"><a href="#Download" rel="noopener noreferrer" className="header__navigation-link">Download</a></li>
+                {/* <li className="header__navigation-item"><a href="#login" target="_blank" rel="noopener noreferrer" className="header__navigation-link">Login</a></li> */}
             </ul>
-        </header>
+        </nav>
     </>
   )
 }
