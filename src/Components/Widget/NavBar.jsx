173aea8c33a9b8572f6d63e781565d283932b8ae
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-06-03 18:20:29 +0200

    ajout burger menu

--- src/Components/Widget/NavBar.jsx
+++ src/Components/Widget/NavBar.jsx
@@ -12,32 +12,32 @@ const NavBar = () => {
   return (
     <>
       <nav className={`header ${isMenuOpen ? 'header--open' : ''}`}>
-        <a href="#Home" className="header__logo">
-          CryptoExtension
-        </a>
-        <ul className={`header__navigation ${isMenuOpen ? 'header__navigation--open' : ''}`}>
-          <li className="header__navigation-item">
-            <a href="#Home" rel="noopener noreferrer" className="header__navigation-link">
-              Home
-            </a>
-          </li>
-          <li className="header__navigation-item">
-            <a href="#Dashboard" rel="noopener noreferrer" className="header__navigation-link">
-              Dashboard
-            </a>
-          </li>
-          <li className="header__navigation-item">
-            <a href="#Download" rel="noopener noreferrer" className="header__navigation-link">
-              Download
-            </a>
-          </li>
-        </ul>
-        <button className={`header__burger ${isMenuOpen ? 'header__burger--open' : ''}`} onClick={handleMenuToggle}>
-          <span className="header__burger-line"></span>
-          <span className="header__burger-line"></span>
-          <span className="header__burger-line"></span>
-        </button>
-      </nav>
+      <a href="#Home" className="header__logo">
+        CryptoExtension
+      </a>
+      <ul className={`header__navigation ${isMenuOpen ? 'header__navigation--open' : ''}`}>
+        <li className="header__navigation-item">
+          <a href="#Home" rel="noopener noreferrer" className="header__navigation-link">
+            Home
+          </a>
+        </li>
+        <li className="header__navigation-item">
+          <a href="#Dashboard" rel="noopener noreferrer" className="header__navigation-link">
+            Dashboard
+          </a>
+        </li>
+        <li className="header__navigation-item">
+          <a href="#Download" rel="noopener noreferrer" className="header__navigation-link">
+            Download
+          </a>
+        </li>
+      </ul>
+      <button className={`header__burger ${isMenuOpen ? 'header__burger--open' : ''}`} onClick={handleMenuToggle}>
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+      </button>
+    </nav>
     </>
   );
 };
