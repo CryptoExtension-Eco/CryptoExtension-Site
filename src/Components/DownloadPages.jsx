01afd12b8f379986985cec6662c3f0ccd5ab4ffa
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-06-04 09:39:46 +0200

    modifs design

--- src/Components/DownloadPages.jsx
+++ src/Components/DownloadPages.jsx
@@ -9,6 +9,7 @@ const DownloadPages = () => {
       <div id='Download'/>
       <section className="download">
         <h1>Download</h1>
+        <a href="https://chrome.google.com/webstore/detail/cryptoextension/jbmpclggkoleafimiibabhiikdfidihf/related" target="_blank" rel="noopener noreferrer">Download</a>
       </section>
       {/* <ChatBot/> */}
       <Footer/>
