8eb1534d20a28b6f2ff459694ea74fc9c716020c
napotwiixe <julien.milants@gmail.com>
2023-06-04 15:29:51 +0200

    add truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    networks: {
+      development: {
+        host: "127.0.0.1",
+        port: 7545, // Le port du réseau de développement (par exemple, Ganache)
+        network_id: "*", // Match any network id
+      },
+      // Ajoutez d'autres réseaux ici si nécessaire
+    },
+  };
+  
