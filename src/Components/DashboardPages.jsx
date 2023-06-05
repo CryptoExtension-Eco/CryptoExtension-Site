9df6652f15d34d4a9054f288ea4c869ab2eae905
napotwiixe <julien.milants@gmail.com>
2023-06-05 12:38:49 +0200

    add balances

--- src/Components/DashboardPages.jsx
+++ src/Components/DashboardPages.jsx
@@ -7,16 +7,20 @@ const contactless = require("../images/wifi-signal.png");
 const DashboardPages = () => {
   const [userWalletAddress, setUserWalletAddress] = useState(null);
   const [loginButtonText, setLoginButtonText] = useState("Connect to your wallet");
+  const [accountBalance, setAccountBalance] = useState(null);
+
+  const handleLogout = () => {
+    setUserWalletAddress(null);
+    setLoginButtonText("Connect to your wallet");
+    setAccountBalance(null);
+  };
 
   return (
     <>
       <div id="Dashboard" />
       <section className="dashboard wallet">
         <div className="contenu">
-          <LoginMetaMask
-            setUserWalletAddress={setUserWalletAddress}
-            setLoginButtonText={setLoginButtonText}
-          />
+          <LoginMetaMask setUserWalletAddress={setUserWalletAddress} setLoginButtonText={setLoginButtonText} setAccountBalance={setAccountBalance} />
           <div id="Wallet">
             <p id="userWallet" className="text-lg text-gray-600 my-2 ethAdresse"></p>
             <div className="card-container">
@@ -25,6 +29,9 @@ const DashboardPages = () => {
                 <img className="contactless" src={contactless} alt="" />
                 <img className="visa" src={visa} alt="" />
                 <p id="test" className="card-number">{userWalletAddress}</p>
+                {userWalletAddress && (
+                <p id="accountBalance">Balance: {accountBalance} ETH</p>
+              )}
                 <div className="arrow"></div>
                 <p className="card-name">Ethereum</p>
                 <p className="bank-name">CryptoBank</p>
@@ -32,7 +39,7 @@ const DashboardPages = () => {
             </div>
             <p className="card-text">Le bouton ci-dessous permet de vous connecter à votre wallet Metamask. Il y aura bientôt la possibilité de choisir n'importe quel wallet.</p>
             <ul>
-              <li><a href="#a" id="loginButton">{loginButtonText}</a></li>
+              <li><a href="#a" id="loginButton" onClick={userWalletAddress ? handleLogout : null}>{loginButtonText}</a></li>
             </ul>
           </div>
 
--- src/Components/js/LoginMetaMask.js
+++ src/Components/js/LoginMetaMask.js
@@ -1,6 +1,7 @@
 import { useEffect} from 'react';
+import Web3 from 'web3';
 
-const LoginMetaMask = ({ setUserWalletAddress, setLoginButtonText }) => {
+const LoginMetaMask = ({ setUserWalletAddress, setLoginButtonText, setAccountBalance }) => {
   useEffect(() => {
     toggleButton();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -11,21 +12,26 @@ const LoginMetaMask = ({ setUserWalletAddress, setLoginButtonText }) => {
   };
 
   const loginWithMetaMask = async () => {
-    const accounts = await window.ethereum
-      .request({ method: "eth_requestAccounts" })
-      .catch(e => {
-        console.error(e.message);
-      });
+    try {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+      
+      if (accounts && accounts.length > 0) {
+        setUserWalletAddress(accounts[0]);
+        setLoginButtonText("Se Déconnecter du Wallet");
+        document.getElementById("loginButton").removeEventListener("click", loginWithMetaMask);
+        setTimeout(() => {
+          document.getElementById("loginButton").addEventListener("click", signOutOfMetaMask);
+        });
 
-    if (accounts && accounts.length > 0) {
-      setUserWalletAddress(accounts[0]);
-      setLoginButtonText("Se Déconnecter du Wallet");
-      document.getElementById("loginButton").removeEventListener("click", loginWithMetaMask);
-      setTimeout(() => {
-        document.getElementById("loginButton").addEventListener("click", signOutOfMetaMask);
-      });
-    } else {
-      console.error("No accounts found.");
+        // Get account balance
+        const web3 = new Web3(window.ethereum);
+        const balance = await getAccountBalance(web3, accounts[0]);
+        setAccountBalance(balance);
+      } else {
+        console.error("No accounts found.");
+      }
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
@@ -36,6 +42,16 @@ const LoginMetaMask = ({ setUserWalletAddress, setLoginButtonText }) => {
     setTimeout(() => {
       document.getElementById("loginButton").addEventListener("click", loginWithMetaMask);
     });
+  };
+
+  const getAccountBalance = async (web3, account) => {
+    try {
+      const balance = await web3.eth.getBalance(account);
+      return web3.utils.fromWei(balance);
+    } catch (error) {
+      console.error("Failed to fetch account balance:", error.message);
+      return null;
+    }
   };
 
   return null;
