e2785bb89d569a2b0b74b3707a066fae68ca04c9
napotwiixe <julien.milants@gmail.com>
2023-06-11 09:59:51 +0200

    fix log error

--- src/Components/Widget/NavBar.jsx
+++ src/Components/Widget/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import '../../scss/Pages/Widgets/Navbar.scss';
 
 const NavBar = () => {
